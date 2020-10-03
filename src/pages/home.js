3fa293742d0ae14f0d180221866d2ac50f839a9e
Muhammed khaled <muhammmed.k95@gmail.com>
2020-10-03 23:57:33 +0200

    remove all warnings

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,6 +1,14 @@
-import React, { Component } from "react";
+import React, { Component, Fragment } from "react";
 import Grid from "@material-ui/core/Grid";
 import PropTypes from "prop-types";
+import withStyles from "@material-ui/core/styles/withStyles";
+
+// MUI stuff
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Paper from "@material-ui/core/Paper";
+
+import { Link } from "react-router-dom";
 
 import Scream from "../components/scream/Scream";
 // import Profile from "../components/profile/Profile";
@@ -9,25 +17,68 @@ import ScreamSkeleton from "../util/ScreamSkeleton";
 import { connect } from "react-redux";
 import { getScreams } from "../redux/actions/dataActions";
 
+const styles = (theme) => ({
+  ...theme,
+});
+
 class home extends Component {
   componentDidMount() {
     this.props.getScreams();
   }
   render() {
-    const { screams, loading } = this.props.data;
+    const {
+      data: { screams, loading },
+      classes,
+    } = this.props;
+    console.log({ classes });
     let recentScreamsMarkup = !loading ? (
       screams.map((scream) => <Scream key={scream.screamId} scream={scream} />)
     ) : (
       <ScreamSkeleton />
     );
+
+    let Login = (
+      <Paper className={classes.paper}>
+        <Typography variant="body2" align="center">
+          No profile found, please login first to create new post
+        </Typography>
+        <div className={classes.buttons}>
+          <Button
+            variant="contained"
+            color="primary"
+            component={Link}
+            to="/login"
+          >
+            Login
+          </Button>
+          <Button
+            variant="contained"
+            color="secondary"
+            component={Link}
+            to="/signup"
+          >
+            Signup
+          </Button>
+        </div>
+      </Paper>
+    );
+    const authenticated = this.props.user.authenticated;
     return (
       <Grid container spacing={16} justify="center">
-        {/* <Grid item sm={4} xs={12}>
-          <Profile />
-        </Grid> */}
-        <Grid item sm={8} xs={12}>
-          {recentScreamsMarkup}
-        </Grid>
+        {authenticated ? (
+          <Grid item sm={8} xs={12}>
+            {recentScreamsMarkup}
+          </Grid>
+        ) : (
+          <Fragment>
+            <Grid item sm={4} xs={12}>
+              {Login}
+            </Grid>
+            <Grid item sm={8} xs={12}>
+              {recentScreamsMarkup}
+            </Grid>
+          </Fragment>
+        )}
       </Grid>
     );
   }
@@ -40,6 +91,9 @@ home.propTypes = {
 
 const mapStateToProps = (state) => ({
   data: state.data,
+  user: state.user,
 });
 
-export default connect(mapStateToProps, { getScreams })(home);
+export default connect(mapStateToProps, { getScreams })(
+  withStyles(styles)(home)
+);
